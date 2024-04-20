15cefca4d36adf0b400031ff8072201d35ffdeed
Dell <asayelh3@gmail.com>
2024-04-20 03:42:36 +0300

    hw

--- src/test/java/assignment/hw3.java
+++ src/test/java/assignment/hw3.java
@@ -2,20 +2,29 @@ package assignment;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
-public class task1 {
+public class hw3 {
+    /*
+        Given
+            https://reqres.in/api/users/3
+        When
+            User sends a GET Request to the url
+        Then
+            HTTP Status Code should be 200
+        And
+            Content Type should be JSON
+        And
+            Status Line should be HTTP/1.1 200 OK
+     */
     public static void main(String[] args) {
 
     Response response = RestAssured.get("https://reqres.in/api/users/3");
 
-    //        HTTP Status Code should be 200
     int statusCode = response.statusCode();
         System.out.println("statusCode = "+statusCode);//200
 
-    //        Content Type should be JSON
     String contentType = response.contentType();
         System.out.println("contentType = "+contentType);
 
-    //        Status Line should be HTTP/1.1 200 OK
     String statusLine = response.statusLine();
         System.out.println("statusLine = "+statusLine);
 }}
