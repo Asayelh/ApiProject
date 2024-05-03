a40d08cf888cdeb3254c64fb680a5f3a70e170b6
Dell <asayelh3@gmail.com>
2024-05-03 18:21:00 +0300

    hw

--- src/test/java/assignment/hw14.java
+++ src/test/java/assignment/hw14.java
@@ -12,38 +12,44 @@ public class hw14 {
 
         private static final String BASE_URL = "https://dummy.restapiexample.com/api/v1/employees";
 
-
         @Test
         public void testEmployeeApi() {
             // Send GET request
             Response response = RestAssured.get(BASE_URL);
-
-            // Verify status code is 200
             assertEquals(200, response.getStatusCode());
 
             // Get the number of employees
             int numberOfEmployees = response.jsonPath().getList("data").size();
             assertEquals(24, numberOfEmployees);
 
-            // Verify "Tiger Nixon" and "Garrett Winters" are among the employees
             assertTrue(response.jsonPath().getList("data.employee_name").contains("Tiger Nixon"));
             assertTrue(response.jsonPath().getList("data.employee_name").contains("Garrett Winters"));
 
             // Find the greatest age
-            int maxAge = response.jsonPath().getList("data.employee_age").stream().mapToInt(Integer::parseInt).max().orElse(0);
+            int maxAge = response
+                    .jsonPath()
+                    .getList("data.employee_age")
+                    .stream().mapToInt(Integer::parseInt)
+                    .max().orElse(0);
             assertEquals(66, maxAge);
 
             // Find the employee with the lowest age
-            String employeeWithLowestAge = response.jsonPath().getList("data.employee_age").stream()
+            String employeeWithLowestAge = response
+                    .jsonPath()
+                    .getList("data.employee_age").stream()
                     .filter(age -> Integer.parseInt((String) age) == 19)
-                    .map(age -> response.jsonPath().getString("data.find { it.employee_age == '" + age + "' }.employee_name"))
+                    .map(age -> response.jsonPath()
+                            .getString("data.find { it.employee_age == '" + age + "' }.employee_name"))
                     .findFirst()
                     .orElse(null);
             assertEquals("Tatyana Fitzpatrick", employeeWithLowestAge);
 
             // Calculate the total salary of all employees
-            int totalSalary = response.jsonPath().getList("data.employee_salary").stream().mapToInt(Integer::parseInt).sum();
+            int totalSalary = response.jsonPath()
+                    .getList("data.employee_salary")
+                    .stream().mapToInt(Integer::parseInt).sum();
             assertEquals(6644770, totalSalary);
+
         }
     }
 
